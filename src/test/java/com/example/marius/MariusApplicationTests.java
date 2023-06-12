afa76023c26a19e7079d8d63a4c0898f0cf70648
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-12 21:25:57 +0200

    initialized project

---
+++ src/test/java/com/example/marius/MariusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
